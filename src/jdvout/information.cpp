b9d8b91e92a12112b1e019976e908c52eb6e60f4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-24 19:15:00 +0100

    Add files via upload

---
+++ src/jdvout/information.cpp
@@ -0,0 +1,19 @@
+void displayInfo() {
+	std::cout << R"(
+
+JPG Data Vehicle (jdvout v1.9). 
+Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
+
+jdvout is a steganography-like CLI tool for extracting hidden data from a (jdvin) "file-embedded" JPG image. 
+
+Compile & run jdvout (Linux):
+		
+$ g++ main.cpp -O2 -lz -s -o jdvout
+$ sudo cp jdvout /usr/bin
+$ jdvout
+
+Usage: jdvout <file-embedded-image>
+       jdvout --info
+
+)";
+}
