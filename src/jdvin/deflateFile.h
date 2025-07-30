78990eecd8b0e3f449582a0110c232e6c390d1ff
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-07-30 17:45:45 +0100

    Delete src/jdvin directory

--- src/jdvin/deflateFile.h
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-#include <vector>
-#include <cstdint>
-
-void deflateFile(std::vector<uint8_t>&, bool);
--- src/jdvin/splitDataFile.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-#include <vector>    
-#include <cstdint>   
-
-void splitDataFile(std::vector<uint8_t>&, std::vector<uint8_t>&, bool&);
-
